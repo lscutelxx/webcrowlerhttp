8c8b2e0957f658ad212cb50540ce546c374f6786
Lscutelx <lscutelx@gmail.com>
2025-03-26 01:43:19 +0300

    normalizeUrl and tests

---
+++ crawl.js
@@ -0,0 +1,10 @@
+function normalizeURL(urlString) {
+  const urlObject = new URL(urlString);
+  const hostPath = `${urlObject.hostname}${urlObject.pathname}`.toLowerCase();
+  if (hostPath.length > 0 && hostPath.slice(-1) === '/') {
+    return hostPath.slice(0, -1);
+  }
+  return hostPath;
+}
+
+module.exports = { normalizeURL };
