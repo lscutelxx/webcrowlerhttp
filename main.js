0cd6d7bbc630f92f8aeebccfd8dcc9355ab16189
Lscutelx <lscutelx@gmail.com>
2025-03-26 20:53:45 +0300

    crawl single page with error handling

--- main.js
+++ main.js
@@ -1,1 +1,17 @@
-console.log('Hello world');
+const { crawlPage } = require('./crawl.js');
+
+function main() {
+  if (process.argv.length < 3) {
+    console.log('no website provided');
+    process.exit(1);
+  }
+  if (process.argv.length > 3) {
+    console.log('too many comand line args');
+    process.exit(1);
+  }
+  const baseUrl = process.argv[2];
+  console.log(`starting crawl of ${baseUrl}`);
+  crawlPage(baseUrl);
+}
+
+main();
